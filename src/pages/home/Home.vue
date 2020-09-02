12fb623651cab512ed522e505b20701f3a0ea6fb
woniu-create <2103029766@qq.com>
2020-09-02 14:21:55 +0800

    finish

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -44,7 +44,6 @@ export default {
     },
     getHomeInfoSucc(res){
       res=res.data
-      console.log(res)
       if(res.ret&&res.data){
         const data=res.data
         this.swiperList=data.swiperList
